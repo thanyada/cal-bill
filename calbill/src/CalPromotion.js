d44bdb2549837f8e2bed3163e22f7f19c575676e
thanyadaKraken <bb.thanyada@gmail.com>
2018-03-29 20:55:21 +0700

    render ui

--- calbill/src/CalPromotion.js
+++ calbill/src/CalPromotion.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Container, Form,Row, Col,Button ,Alert,Fade, FormGroup,Label,Input,FormFeedback,FormText
+import { Container, Form,Row, Col,Button ,Alert,Fade, FormGroup,Label,Input,FormFeedback,FormText,InputGroup,InputGroupAddon
 } from 'reactstrap';
 
 
@@ -9,11 +9,11 @@ class CalPromotion extends Component {
 
 constructor(props) {
         super(props);
-        this.state = { fadeIn: false };
-
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+        const PRICE_PER_PERSON = 459
+        this.state = { fadeIn: false,promotion:{}};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        
 
     }
 
@@ -26,27 +26,102 @@ constructor(props) {
 
     console.log( this.state.value +' and '+ validData )
     
+    if(validData) alert("Invalid Data Count of customer")
+
+    var pm =  this.CalPromotion(this.state.value)
+
     this.setState({
-            fadeIn: !validData
+            fadeIn: !validData,
+            promotion : pm
     });
       
       event.preventDefault();
   }
 
 
+CalPromotion(count_customer){
+
+  var promotion = {promotions:[],promotion_title:"", origin_price:0, discount_price:0,price_per_person:0};
+  var price = count_customer*this.PRICE_PER_PERSON
+
+  promotion.origin_price = price
+
+  
+  if(count_customer == 1){
+    promotion.promotion_title = "You not have Promotion"
+  }else{
+    promotion.promotion_title = "You should this Promotion"
+    var array_promotion = []
+
+    if(count_customer < 4){
+      array_promotion.push("LUCKY TWO")
+
+    }else{
+      if(count_customer / 4 >0){
+        if(count_customer / 4==1){
+          array_promotion.push("4PAY3")
+        }else  array_promotion.push("4PAY3 ("+count_customer%4+" Sets )")
+       
+      }
+
+      if((count_customer%4) >= 2 &&  (count_customer%4) / 2 > 0){
+        array_promotion.push("LUCKY TWO")
+      }
+
+      if(count_customer > 13){
+        array_promotion.push("Discount 25%")
+      }
+    }
+
+    promotion.promotions = array_promotion
+  }
+ 
+  return promotion
+  
+}
+
+
 
   render() {
+    var lis = [];
+    
+
+    for (var key in this.state.promotion.promotions) {
+        var element = this.state.promotion.promotions[key];
+      lis.push(<p>{element}</p>);
+     
+    }
+
     return (
       <div  className="App-Main">
 
       <Form onSubmit={this.handleSubmit}>
-            <label>
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            
-            <input type="submit" value="Submit" />
-      
-          <Fade in={this.state.fadeIn} tag="h5" className="mt-3">This content will {this.state.value}</Fade>
+
+        <FormGroup>
+            {/*<Input className="Stlye-Input" type="number" value={this.state.value} onChange={this.handleChange} placeHolder="Count of customer"/>*/}
+           
+           <label>
+              <InputGroup className="Stlye-Input" >
+              <InputGroupAddon addonType="prepend" >Count of Customer</InputGroupAddon>
+              <Input invalid={!this.state.fadeIn} type="number" value={this.state.value} onChange={this.handleChange} />
+            </InputGroup>
+             </label>
+            <Button className="Stlye-botton" color="info">Calculate</Button>
+        </FormGroup>
+
+          <Fade in={this.state.fadeIn} tag="h5" className="mt-3">
+            <Alert>
+            <h3> {this.state.promotion.promotion_title}</h3>
+            {lis}
+            </Alert>
+
+             <Alert color="danger">
+            <h3> Price</h3>
+            <p>Original Price : {this.state.promotion.origin_price}</p>
+            <p>Discount Price : {this.state.promotion.discount_price}</p>
+            <p>Price per person : {this.state.promotion.price_per_person}</p>
+            </Alert>
+          </Fade>
 
       </Form>
 
